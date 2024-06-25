b3a71087e0f68e1696bb84a29b7d8d207f9096f1
Firoz <136556894+firoz1860@users.noreply.github.com>
2024-06-25 10:04:39 +0530

    first commit

---
+++ 3-myntra-react-clone/src/components/FetchItems.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { itemsActions } from "../store/itemsSlice";
+import { fetchStatusActions } from "../store/fetchStatusSlice";
+
+const FetchItems = () => {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (fetchStatus.fetchDone) return;
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    dispatch(fetchStatusActions.markFetchingStarted());
+    fetch("http://localhost:8080/items", { signal })
+      .then((res) => res.json())
+      .then(({ items }) => {
+        dispatch(fetchStatusActions.markFetchDone());
+        dispatch(fetchStatusActions.markFetchingFinished());
+        dispatch(itemsActions.addInitialItems(items[0]));
+      })
+      .catch((error) => {
+        if (error.name !== 'AbortError') {
+          console.error('Error fetching items:', error);
+          dispatch(fetchStatusActions.markFetchingFinished());
+        }
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [fetchStatus]);
+
+  return <></>;
+};
+
+export default FetchItems;
